ea8d2ec117ef444c408cb0f4a03da6bb372664fa
kushalkhandhara <kkhandhara@gmail.com>
2025-04-09 21:49:54 +0530

    url added

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5001;
 
 // Allow credentials and specify the origin explicitly
 const corsOptions = {
-  origin: 'http://localhost:5173', // This should match the frontend's origin
+  origin: process.env.CLIENT_BASE_URL, // This should match the frontend's origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
   credentials: true, // Allow cookies and authentication credentials
